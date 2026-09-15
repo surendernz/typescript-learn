6fb3bd71fe069f24bf9cee216fc44b1cfb17e5d8
surendernz <surenderreddy23@gmail.com>
2026-09-15 00:49:19 +0000

    generics2

--- src/generics.ts
+++ src/generics.ts
@@ -1,11 +1,18 @@
 let names: Array<string> = ['aaa','bbb'];
+// this is an existing generic type
 
-type DataStore<T> = {
+
+//this is a custom generic type
+type DataStore<T> = { //T is a placeholder type which can be set later
     [key:string]:T
 }
+// dynamically setting the type value
 let store : DataStore<string| number>= {};
 
-store.username = 'sss';
-store.age = 10;
+store.username = 'sss'; // this is string value
+store.age = 10; // this is a number value
 
+let a : DataStore<string> = {}
+a.a = 'aaa';
+a.b = 'bbb';
 
